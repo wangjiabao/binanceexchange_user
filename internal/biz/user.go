7d1ffc2e066f223f34c417d8e8ee0610efe6e9f6
wangjiabao <840028401@qq.com>
2024-02-17 16:58:24 +0800

    ok

--- internal/biz/user.go
+++ internal/biz/user.go
@@ -1204,9 +1204,9 @@ func requestBinanceOrderHistory(apiKey string, secretKey string, symbol string, 
 	)
 
 	// 时间
-	now := strconv.FormatInt(time.Now().UTC().UnixMilli(), 10)
+	//now := strconv.FormatInt(time.Now().UTC().UnixMilli(), 10)
 	// 拼请求数据
-	data = "symbol=" + symbol + "&timestamp=" + now
+	data = "symbol=" + symbol + "startTime=" + startTime + "&endTime=" + endTime + "&limit=10"
 	fmt.Println(data)
 	// 加密
 	h := hmac.New(sha256.New, []byte(secretKey))
