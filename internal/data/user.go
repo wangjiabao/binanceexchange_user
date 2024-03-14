82206eef35a5610bc0752e463942a929e24bac6f
wangjiabao <840028401@qq.com>
2024-03-14 17:52:17 +0800

    ok

--- internal/data/user.go
+++ internal/data/user.go
@@ -1521,7 +1521,7 @@ func (b *BinanceUserRepo) GetUserBindTraderTwoByTraderIds(traderIds []uint64) (m
 func (b *BinanceUserRepo) GetUserBindAfterUnbindByUserIdAndTraderIdAndSymbol(ctx context.Context, userId uint64, traderId uint64, symbol string, positionSide string) (*biz.UserBindAfterUnbind, error) {
 	var userBindAfterUnbind *UserBindAfterUnbind
 	if err := b.data.DB(ctx).Table("user_bind_after_unbind").
-		Where("userId=? and trader_id=? and status=? and symbol=? and position_side=?", userId, traderId, 0, symbol, positionSide).
+		Where("user_id=? and trader_id=? and status=? and symbol=? and position_side=?", userId, traderId, 0, symbol, positionSide).
 		First(&userBindAfterUnbind).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, nil
